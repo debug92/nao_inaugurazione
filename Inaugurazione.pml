75dd7efb978c4df9a5be01c03b5c91a39703ee2c
Mario Protopapa <mario.protopapa@gmail.com>
2022-02-03 09:45:37 +0100

    srcs Added

---
+++ Inaugurazione.pml
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Inaugurazione" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources />
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="it_IT">
+        <Translation name="translation_it_IT" src="translations/translation_it_IT.ts" language="it_IT" />
+    </Translations>
+</Package>
---
+++ translations/translation_it_IT.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Benvenuti nella prima biblioteca comunale. Vi prego di mantenere le distanze di sicurezza. Buon viaggio tra passato e futuro.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Benvenuti nella prima biblioteca comunale. Vi prego di mantenere le distanze di sicurezza. Buon viaggio tra passato e futuro.</translation>
+        </message>
+        <message>
+            <source>Benvenuti nella prima biblioteca comunale di Brindisi. Vi prego di mantenere le distanze di sicurezza. Buon viaggio tra passato e futuro.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Benvenuti nella prima biblioteca comunale di Brindisi. Vi prego di mantenere le distanze di sicurezza. Buon viaggio tra passato e futuro.</translation>
+        </message>
+        <message>
+            <source>Benvenuti nella prima biblioteca comunale di Brindisi. Vi prego di mantenere le distanze di sicurezza, buon viaggio tra passato e futuro.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Benvenuti nella prima biblioteca comunale di Brindisi. Vi prego di mantenere le distanze di sicurezza, buon viaggio tra passato e futuro.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Benvenuti nella prima biblioteca comunale di Brindisi. Durante questo percorso vi prego di mantenere le distanze di sicurezza. Vi auguro un buon viaggio tra passato e futuro.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Benvenuti nella prima biblioteca comunale di Brindisi. Durante questo percorso vi prego di mantenere le distanze di sicurezza. Vi auguro un buon viaggio tra passato e futuro.</translation>
+        </message>
+    </context>
+</TS>
